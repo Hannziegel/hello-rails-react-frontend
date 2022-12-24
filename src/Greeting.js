e3e4825ba46540077d53b4d8b55c154775657fd4
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-12-24 19:16:11 -0300

    Install and fix linters

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getGreetings } from './reducers/greetings'
+import { getGreetings } from './reducers/greetings';
 
 const Greeting = () => {
   const dispatch = useDispatch();
   const greeting = useSelector((state) => state.greetings.greetings);
   useEffect(() => {
     if (!greeting.length) dispatch(getGreetings());
-    console.log(greeting)
   });
   function refreshPage() {
     window.location.reload();
@@ -15,11 +14,11 @@ const Greeting = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div onClick={refreshPage} className="p-12 max-w-sm mx-auto bg-cyan-700 rounded-xl shadow-lg hover:shadow-2xl hover:shadow-white hover:scale-20 shadow-white flex items-center space-x-4 cursor-pointer">
+      <div role="presentation" onClick={refreshPage} onKeyDown={refreshPage} className="p-12 max-w-sm mx-auto bg-cyan-700 rounded-xl shadow-lg hover:shadow-2xl hover:shadow-white hover:scale-20 shadow-white flex items-center space-x-4 cursor-pointer">
         <h2 className="text-xl font-xl text-white">{greeting}</h2>
       </div>
     </div>
-  )
+  );
 };
 
 export default Greeting;
